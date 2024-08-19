392b6939ce716c3148b6d36e1799e519dc8ecfb0
Ghost0159 <contact.ghost01@gmail.com>
2024-08-19 08:45:02 +0200

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://wiki.ghosteshop.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Ghost-eShop-Wiki/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
